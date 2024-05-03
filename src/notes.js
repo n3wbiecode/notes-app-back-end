f7e0c8f2d6d88394b5d692d1f481cef01d0799dd
Fatur Rahman <reyharajuku20@gmail.com>
2024-05-03 21:16:43 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,10 @@
+const notes = [{
+    id: "string",
+    title: "string",
+    createdAt: "string",
+    updatedAt: "string",
+    tags: ["string"],
+    body: "string",
+}];
+
+module.exports = notes;
